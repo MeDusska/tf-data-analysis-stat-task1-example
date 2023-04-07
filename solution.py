ce54a22626709c46dd409c3eeeb56bc5f7c4091e
MeDusska <129972403+MeDusska@users.noreply.github.com>
2023-04-07 18:29:46 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 493548023 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> float:
+def solution(x: np.array) -> tuple:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    left = x.max()
+    right = x.max()/((1-p)**(1/len(x)))
+    return left, right
